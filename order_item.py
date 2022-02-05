1ad2490680fcbf7429732ac0e31c6740c78e615d
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-02-04 19:48:15 -0800

    feature: add class order item to represents an order item

---
+++ order_item.py
@@ -0,0 +1,15 @@
+class OrderItem:
+
+    def __init__(self, name, quantity, price):
+        self.name = name
+        self.__quantity = quantity
+        self.__price = price
+
+    def get_price(self):
+        return self.__quantity * self.__price
+
+    def increase_quantity(self):
+        self.__quantity = self.__quantity - 1
+
+    def decrease_quantity(self):
+        self.__quantity = self.__quantity + 1
