cf19499215255eac5ec99553162dc0f4f5c4d276
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-02-05 20:20:11 -0800

    refact: add return type to methods in order item class

--- order_item.py
+++ order_item.py
@@ -1,15 +1,18 @@
+from tkinter.messagebox import NO
+
+
 class OrderItem:
 
-    def __init__(self, name, quantity, price):
+    def __init__(self, name, quantity, price) -> None:
         self.name = name
         self.__quantity = quantity
         self.__price = price
 
-    def get_price(self):
+    def get_price(self) -> float:
         return self.__quantity * self.__price
 
-    def increase_quantity(self):
+    def increase_quantity(self) -> None:
         self.__quantity = self.__quantity + 1
 
-    def decrease_quantity(self):
+    def decrease_quantity(self) -> None:
         self.__quantity = self.__quantity - 1
